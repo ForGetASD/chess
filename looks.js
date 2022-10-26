c72861702d7a1976e07c7bdf20105f733e1c0319
ForGetASD <vitayz60@gmail.com>
2022-10-26 09:22:03 +0200

    v0.013 lehelyezés müködik viszont hibásan

--- looks.js
+++ looks.js
@@ -57,13 +57,16 @@ function coloring(){
 }
 
 function piecesPlacements(){
+    let szam = 0
     for (let i = 0; i < chessBoard.length; i++) {
         for (let j = 0; j < chessBoard.length; j++) {   
             if(String(chessBoard[i][j]) != "-"){
                 if(String(chessBoard[i][j]) === String(chessBoard[i][j]).toUpperCase()){
-                    thePieceColor("white", chessBoard[i][j], i, j)
+                    thePieceColor("white", chessBoard[i][j], szam)
+                    szam++
                 }else{
-                    thePieceColor("dark", chessBoard[i][j], i, j)
+                    thePieceColor("dark", chessBoard[i][j], szam)
+                    szam++
                 }
             }
         }
@@ -71,38 +74,42 @@ function piecesPlacements(){
 }
 
 
-function thePieceColor(color, babu, iPozicio, jPozicio){
+function thePieceColor(color, babu, pozicio){
     let tomb = []
+    let tszam = 0
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            
+            tomb[tszam] = chessBoard[i][j]
+            tszam++
         }
     }
+
+    let poz = $(".square")
 
     var image = new Image();
         if(babu == "r" || babu == "R"){
             var url = './pics/' + color + 'Rook.png'
             image.src = url
-            poz[iPozicio][jPozicio].append(image)
+            poz[pozicio].append(image)
         }else if(babu == "n" || babu == "N"){
             var url = './pics/' + color + 'Knight.png'
             image.src = url
-            poz[iPozicio][jPozicio].append(image)
+            poz[pozicio].append(image)
         }else if(babu == "b" || babu == "B"){
             var url = './pics/' + color + 'Bishop.png'
             image.src = url
-            poz[iPozicio][jPozicio].append(image)
+            poz[pozicio].append(image)
         }else if(babu == "q" || babu == "Q"){
             var url = './pics/' + color + 'Queen.png'
             image.src = url
-            poz[iPozicio][jPozicio].append(image)
+            poz[pozicio].append(image)
         }else if(babu == "k" || babu == "K"){
             var url = './pics/' + color + 'King.png'
             image.src = url
-            poz[iPozicio][jPozicio].append(image)
+            poz[pozicio].append(image)
         }else if(babu == "p" || babu == "P"){
             var url = './pics/' + color + 'Pawn.png'
             image.src = url
-            poz[iPozicio][jPozicio].append(image)
+            poz[pozicio].append(image)
         }
 }
